3f8cd8f68447dbcacb4f2666d87315cd9d5735a7
ChrisOhm <ccohman@gmail.com>
2022-12-05 22:23:02 +0100

    Created Description and meta code.

---
+++ main.py
@@ -0,0 +1,15 @@
+"""Developer Programming test / Submarine bingo Subsystem
+
+A developer programming test consisting of a Submarine Bingo Subsystem that makes sure that a giant squid wins at
+bingo by always picking the worst / loosing bingo board. """
+__author__ = 'Christoffer Öhman'
+
+
+# Load draw numbers
+# Load bingo Boards (5x5 matrix)
+# Evaluate bingo boards and find/ return the worst one and or it's score
+# Run test input, unit test?
+# Run real input data
+# Varify score with Krea API
+
+
